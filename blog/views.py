233dff26c7686057da54d35a7517c9fc20378dcc
ignaciotrucco <truccoignacio9@gmail.com>
2025-04-06 23:11:21 -0300

    vista con datos en BD

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+def post_list(request):
+    post_list = Post.objects.all()
+    return render(request, 'blog/blog_list.html', context={"posts": post_list})
